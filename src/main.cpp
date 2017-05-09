2feb3e74f2ddbf6cce41d39e623ea8d3ac1dd25d
Pierre <pier.charles@hotmail.fr>
2017-05-09 14:03:52 +0200

    Merge with my work

--- src/main.cpp
+++ src/main.cpp
@@ -6,12 +6,5 @@ int main() {
 
     DBIntecractor::getInstance()->InsertDefaultData("../data/data.sql");
 
-    if (!DBInteractor::getInstance()) {
-        std::cout << "error" << std::endl;
-        return 1;
-    }
-    else {
-        std::cout << "ok" << std::endl;
-       return 0;
-    }
+
 }
